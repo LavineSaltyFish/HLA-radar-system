073861cc58900b140dc98204bb7d0033febce638
LavineSaltyFish <52395844+LavineSaltyFish@users.noreply.github.com>
2022-06-22 14:12:14 +0200

    Delete buildDetectionResult.m

--- buildDetectionResult.m
+++
@@ -1,9 +0,0 @@
-function [ DetectionResult,jobq ] = buildDetectionResult( flag,detection,jobq)
-if flag
-DetectionResult = detection.detection;
-else
-    DetectionResult=[];
-end
-jobq.Flag = flag;
-end
-
